589dcb734ba6d54b583c013d0324a35edc59a07f
Shuaib Parker <parkershuaib@gmail.com>
2020-07-20 12:51:28 +0200

    Removed dimension selection from frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { FileSelector } from './components/FileSelector'
 import './App.css';
-import {InputGroup } from "@blueprintjs/core";
 
 const { FileDetails, FilesRequest } = require('./voxualize-protos/helloworld_pb.js');
 const { GreeterClient } = require('./voxualize-protos/helloworld_grpc_web_pb.js');
@@ -10,9 +9,6 @@ const { GreeterClient } = require('./voxualize-protos/helloworld_grpc_web_pb.js'
 function App() {
   const [message, setMessage] = useState('')
   const [filename, setFileName] = useState('')
-  const [dimensionx, setDimensionx] = useState('')
-  const [dimensiony, setDimensiony] = useState('')
-  const [dimensionz, setDimensionz] = useState('')
   const [filenames, setFileNames] = useState([])
 
   var client = new GreeterClient('http://' + window.location.hostname + ':8080', null, null);
@@ -24,9 +20,6 @@ function App() {
     else {
       var request = new FileDetails();
       request.setFileName(filename);
-      request.setDimensionx(dimensionx);
-      request.setDimensiony(dimensiony);
-      request.setDimensionz(dimensionz);
       client.chooseFile(request, {}, (err: any, response: any) => {
         if (response) { 
 
@@ -59,11 +52,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <FileSelector files={filenames} name={"Choose a file ..."} onClick={requestFiles} onItemSelected={(file: any) => {setFileName(file)}} />
-        <div className="d-flex flex-column my-4 ">
-          <InputGroup  placeholder={"Enter x coordinate"} value={dimensionx} onChange={(event:any) => setDimensionx(event.target.value)}  name="X:" />
-          <InputGroup  placeholder={"Enter y coordinate"} value={dimensiony} onChange ={(event:any) => setDimensiony(event.target.value)}  name="Y:" />
-          <InputGroup  placeholder={"Enter z coordinate"} value={dimensionz} onChange={(event:any) => setDimensionz(event.target.value)}  name="Z:" />
-        </div>
         <h5>{message}</h5>
         <button className="btn btn-success" onClick={callGrpcService}>Render</button>
 
